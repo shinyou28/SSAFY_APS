921d39a926c7ccae9c90528132ac04830303cc5f
a99yyoung <a99yyoung@naver.com>
2024-07-30 13:31:48 +0900

    0729 homework

--- day01_0729월/src/SWEA_1984_중간평균값구하기/Solution.java
+++ day01_0729월/src/SWEA_1984_중간평균값구하기/Solution.java
@@ -1,5 +1,36 @@
 package SWEA_1984_중간평균값구하기;
 
-public class Solution {
+import java.util.Scanner;
 
+class Solution {
+    public static void main(String args[]) throws Exception {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        int[] arr = new int[10];
+ 
+        for (int i = 0; i < T; i++) {
+            for (int j = 0; j < arr.length; j++) {
+                arr[j] = sc.nextInt(); // 10개의 수 입력
+            }
+ 
+            int max = 0;
+            int min = 10000;
+            int sum = 0;
+ 
+            for (int j = 0; j < arr.length; j++) {
+                sum += arr[j];
+ 
+                if (arr[j] > max) {
+                    max = arr[j]; // 최대값
+                }
+                if (arr[j] < min) {
+                    min = arr[j]; // 최소값
+                }
+            }
+ 
+            double avg = (double) (sum - max - min) / (arr.length - 2);
+            System.out.println("#" + (i + 1) + " " + Math.round(avg));
+        }
+ 
+    }
 }
--- day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
+++ day01_0729월/src/SWEA_2068_최대수구하기/Solution.java
@@ -1,5 +1,33 @@
 package SWEA_2068_최대수구하기;
 
-public class Solution {
-
+import java.util.Scanner;
+ 
+class Solution
+{
+    public static void main(String args[]) throws Exception
+    {
+        Scanner sc = new Scanner(System.in);
+         
+        int T = sc.nextInt();
+        int[] arr = new int[10];
+        int max = 0;
+         
+        for(int i = 0; i < T; i++) {
+             
+            for(int j = 0; j < arr.length; j++) {
+                arr[j] = sc.nextInt(); // 10개의 수 입력
+            }
+             
+            for(int j = 0; j < arr.length; j++) {
+                if(arr[j]>max) {
+                    max = arr[j];
+                }
+            }
+             
+            System.out.println("#"+(i+1)+" "+max);
+            max = 0;
+             
+        }
+         
+    }
 }
