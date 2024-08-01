71567a95a8932d6dfa7080a1ef91bd13863842c7
shinyou28 <shinyou0828@gmail.com>
2024-08-01 15:13:01 +0900

    0801 homework

--- day03_0731수/src/SWEA_1215_회문1/Solution.java
+++ day03_0731수/src/SWEA_1215_회문1/Solution.java
@@ -1,5 +1,49 @@
 package SWEA_1215_회문1;
 
-public class Solution {
+import java.util.Arrays;
+import java.util.Scanner;
 
+public class Solution {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		char[][] arr = new char[8][8]; // 8X8 배열
+		int length = sc.nextInt(); // 찾아야 하는 회문의 길이	
+		int cnt = 0; // 찾은 회문의 갯수
+		
+		for(int i = 0; i < 8; i++) {
+			String word = sc.next();
+			for(int j = 0; j < 8; j++) {
+				arr[i][j] = word.charAt(j); // 8x8 배열에 글자 입력
+			}
+		}
+		
+		// 1행부터 8행까지 회문 찾기
+		for(int i = 0; i < 8; i++) {
+			for(int j = 0; j <= 8-length; j++) {
+				if(arr[i][j] == arr[i][j+(length-1)]) {
+						cnt += 1;
+						continue;
+				} else {
+						cnt = 0;
+						break;
+				}
+			}
+		}
+		
+		// 1열부터 8열까지 회문 찾기
+		for(int i = 0; i <= 8-length; i++) {
+			for(int j = 0; j <= 8; j++) {
+				if(arr[i][j] == arr[i+(length-1)][j]) {
+						cnt += 1;
+						continue;
+				} else {
+						cnt = 0;
+						break;
+				}
+			}
+		}
+
+		System.out.println(cnt);
+	}
 }
---
+++ day04_0801목/src/SWEA_2001_파리퇴치/Solution.java
@@ -0,0 +1,43 @@
+package SWEA_2001_파리퇴치;
+
+import java.util.Scanner;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt(); // 테스트 케이스 갯수
+
+        for (int tc = 1; tc <= T; tc++) {
+            int N = sc.nextInt(); // 배열의 크기
+            int M = sc.nextInt(); // 파리채의 크기
+            int max = 0; // 파리채로 최대로 많이 잡을 수 있는 파리의 갯수
+
+            int[][] arr = new int[N][N];
+
+            // NXN 배열에 들어가는 파리의 갯수 입력
+            for (int i = 0; i < N; i++) {
+                for (int j = 0; j < N; j++) {
+                    arr[i][j] = sc.nextInt();
+                }
+            }
+
+            // MxM 파리채로 잡을 수 있는 최대 파리 수 찾기
+            for (int i = 0; i <= N - M; i++) {
+                for (int j = 0; j <= N - M; j++) {
+                    int sum = 0;
+                    for (int x = i; x < i + M; x++) {
+                        for (int y = j; y < j + M; y++) {
+                            sum += arr[x][y];
+                        }
+                    }
+                    if (sum > max) {
+                        max = sum;
+                    }
+                }
+            }
+            // 결과 출력
+            System.out.println("#" + tc + " " + max);
+        }
+
+    }
+}
