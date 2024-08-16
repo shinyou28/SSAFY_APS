d19ef41cf99622611999e29193bbc754c7842f0d
shinyou28 <shinyou0828@gmail.com>
2024-08-16 10:53:42 +0900

    모의고사

--- SW_역량_테스트_모의고사/src/보물상자_비밀번호.java
+++ SW_역량_테스트_모의고사/src/보물상자_비밀번호.java
@@ -9,16 +9,16 @@ public class 보물상자_비밀번호 {
             int N = sc.nextInt(); // 숫자의 길이 입력
             int K = sc.nextInt(); // 찾고자 하는 순위 입력
              
-            int rotate = N / 4; // 한 변의 길이 (회전할 때 자를 부분의 길이)
+            int rotate = N / 4; // 한 변의 길이
             String num = sc.next(); // 16진수 문자열 입력
             // 중복 되지 않은 16진수 문자열 저장
-            Set<String> uniqueNumber = new HashSet<>();
+            Set<String> uniqueNum = new HashSet<>();
             // 회전 진행
             for (int r = 0; r < rotate; r++) {
  
                 for (int i = 0; i < 4; i++) {
                     String part = num.substring(i*rotate, (i+1)*rotate);
-                    uniqueNumber.add(part);
+                    uniqueNum.add(part);
                 }
  
                 // 문자열을 시계 방향으로 회전
@@ -27,14 +27,14 @@ public class 보물상자_비밀번호 {
             }
  
             // set에 있는 16진수를 10진수로 변경
-            List<Integer> sortedNumber = new ArrayList<>();
-            for(String s : uniqueNumber) {
-                sortedNumber.add(Integer.parseInt(s, 16));
+            List<Integer> tenNum = new ArrayList<>();
+            for(String s : uniqueNum) {
+            	tenNum.add(Integer.parseInt(s, 16));
             }
  
-            Collections.sort(sortedNumber, Collections.reverseOrder());
+            Collections.sort(tenNum, Collections.reverseOrder());
  
-            System.out.println("#" + tc + " " + sortedNumber.get(K-1));
+            System.out.println("#" + tc + " " + tenNum.get(K-1));
              
         }
  
